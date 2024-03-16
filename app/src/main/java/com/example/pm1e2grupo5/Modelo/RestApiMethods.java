cab8bfb22f207b4ce3bc12d187f33382af6a87a0
CJPF23 <carlosj.perezf.23@gmail.com>
2024-03-15 23:48:57 -0600

    crea contacto en base de datos
    crea contacto en base de datos

---
+++ app/src/main/java/com/example/pm1e2grupo5/Modelo/RestApiMethods.java
@@ -0,0 +1,10 @@
+package com.example.pm1e2grupo5.Modelo;
+
+public class RestApiMethods {
+  //  public static final String Endpointplace = "https://jsonplaceholder.typicode.com/posts";
+
+   // public static final String EndpointGetPerson = "http://192.168.100.10/PM01RestAPI/getperson.php";
+
+    public static final String EndpointPostContacto = "http://192.168.1.39/PM1E2Grupo5Api/CreateContacto.php";
+
+}
